301f0eb73eda792defb80e723389da19f6e42cd0
Aldair <aldairtorres507@gmail.com>
2024-10-11 18:53:37 -0500

    cosas nuevas

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST= process.env.DB_HOST || 'mysql.railway.internal'
-export const DB_USER= process.env.DB_USER || 'root'
-export const DB_PASSWORD= process.env.DB_PASSWORD || 'xdkfpUyhZBLVdIKOHMgoCInjJDshrYNr'
-export const DB_NAME=process.env.DB_NAME || 'railway'
-export const DB_PORT=process.env.DB_PORT || 29452
+export const DB_HOST = process.env.DB_HOST || 'autorack.proxy.rlwy.net';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'xdkfpUyhZBLVdIKOHMgoCInjJDshrYNr';
+export const DB_NAME = process.env.DB_NAME || 'railway';
+export const DB_PORT = process.env.DB_PORT || 29452;
