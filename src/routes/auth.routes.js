a87df038bac75fc36dea1fe78578e6e3fb29ee14
Aldair <aldairtorres507@gmail.com>
2024-10-15 20:22:33 -0500

    crud completo 8erdsww

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { login, logout, register, verify } from "../controllers/auth.controllers.js";
+import {
+  login,
+  logout,
+  register,
+  verify,
+} from "../controllers/auth.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { loginSchema, registerSchema } from "../schemas/validateSchema.js";
 import { authRequiered } from "../middlewares/validateToken.js";
@@ -9,7 +14,6 @@ const router = Router();
 router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
-    router.get('/verify' ,verify)
-
+router.get("/verify", verify);
 
 export default router;
